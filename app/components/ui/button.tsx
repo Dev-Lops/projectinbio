124b30f296678978ae0a73be9d9afe532cbe1e85
Anderson Lopes <developesanderson@gmail.com>
2025-01-13 17:44:56 -0400

    ✅ - Create a button component

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/app/lib/utils";
+
+export default function Button({
+  children,
+  variant = "primary",
+  ...props
+}: {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" | "ghost";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button {...props}
+      className={cn("text-white p-3 rounded-xl whitespace-nowrap hover:opacity-95 disabled:opacity-70 disabled:cursor-not-allowed",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secondary" && "bg-background-tertiary",
+        variant === "ghost" && "bg-border-primary",
+        props.className
+      )} >
+      {children}
+    </button>
+  );
+}
