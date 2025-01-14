a7414188cb075ca1e239c40a148001965b8bff43
Anderson Lopes <developesanderson@gmail.com>
2025-01-13 21:09:11 -0400

    ✅ - Create a textarea and text input component

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,8 +1,11 @@
+import { TotalVisits } from "../commons/total-visits";
+import UserCard from "../commons/user-card";
 import Button from "../ui/button";
+import TextInput from "../ui/text-input";
 
 export default function Hero() {
   return (
-    <div className="flex border">
+    <div className="flex h-screen">
       <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-white text-5xl font-bold leading-[64px]">Seus projetos e redes sociais em um único link</h1>
         <h2 className="text-xl leading-6">Crie sua própria pagina de projetos e compartilhe eles com o mundo.
@@ -10,16 +13,16 @@ export default function Hero() {
           Acompanhe o engajamento com o Analytics de cliques e acessos.
         </h2>
         <div className="flex items-center gap-2 w-full mt-[10vh]">
-          <span className="text-white text-xl">projectinbio.com</span>
-          <input type="text" />
+          <span className="text-white text-xl">projectinbio.com/</span>
+          <TextInput type="text" placeholder="Seu link" />
           <Button>Criar agora</Button>
         </div>
       </div>
       <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4b2dbb,transparent_55%)]">
         <div className="relative">
-          {/* <UserCard /> */}
-          <div className="absolute -bottom=[7%] -right-[45%]">
-            {/* <TotalVisits /> */}
+          <UserCard />
+          <div className="absolute -bottom-[7%] -right-[45%]">
+            <TotalVisits />
           </div>
           <div className="absolute top-[20%] -left-[45%] -z-10">
             {/* <ProjectCard /> */}
@@ -28,7 +31,7 @@ export default function Hero() {
             {/* <ProjectCard /> */}
           </div>
         </div>
-        
+
       </div>
     </div>
   );
